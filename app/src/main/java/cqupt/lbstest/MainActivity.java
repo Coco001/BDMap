1d24f03d04361f396c6d352e12b77e95b9d0011f
Coco001 <wsgkk000@163.com>
2017-02-12 09:02:22 +0800

    MainActivity

--- app/src/main/java/cqupt/lbstest/MainActivity.java
+++ app/src/main/java/cqupt/lbstest/MainActivity.java
@@ -26,6 +26,7 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private LocationClient mLocationClient;
     private MapView mapView;
     private BaiduMap map;
